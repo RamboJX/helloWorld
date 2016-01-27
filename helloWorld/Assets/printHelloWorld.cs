ac3e0209df841b8d524a90d13b47eb7327139fa2
Rambo Wang <lingbo.wong@gmail.com>
2016-01-27 17:56:28 +0800

    helloword first commit

---
+++ helloWorld/Assets/printHelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class printHelloWorld : MonoBehaviour {
+	public Text hello;
+	// Use this for initialization
+	void Start () {
+		hello.text = "Hello World";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
